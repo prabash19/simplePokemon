d6e7f51516871f2f91df6bc48d0f5c5d15413d2e
prabash19 <prabash19@gmail.com>
2024-04-08 08:14:14 +0545

    add and remove functionality done

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,19 @@
 import Navbar from "../navbar/Navbar";
-import { saveAsFavourites } from "../../helpers/localStorage";
+import {
+  saveAsFavourites,
+  getLocalStorageValue,
+  removeFromFavourites,
+} from "../../helpers/localStorage";
+import { useEffect, useState } from "react";
 function Home({ data, previousPage, nextPage, setCurrentPage }: any) {
-  console.log("pokemon data is", data);
+  const [favs, setFavs] = useState<number[] | null>([]);
+  const updateStorageValues = () => {
+    setFavs(getLocalStorageValue());
+  };
+  useEffect(() => {
+    setFavs(getLocalStorageValue());
+  }, []);
+  console.log("favs is", favs);
   return (
     <Navbar>
       <>
@@ -10,13 +22,35 @@ function Home({ data, previousPage, nextPage, setCurrentPage }: any) {
             <div style={{ display: "flex" }} key={item.name}>
               name: <li>{item.name}</li>
               weight: <li>{item.weight}</li>
-              <button onClick={() => saveAsFavourites(item.id)}>add</button>
+              id:<li>{item.id}</li>
+              {favs?.includes(item.id) ? (
+                <button
+                  onClick={() => {
+                    removeFromFavourites(item.id);
+                    updateStorageValues();
+                  }}
+                >
+                  remove
+                </button>
+              ) : (
+                <>
+                  <button
+                    onClick={() => {
+                      saveAsFavourites(item.id);
+                      updateStorageValues();
+                    }}
+                  >
+                    add
+                  </button>
+                </>
+              )}
             </div>
           ))}
         </ul>
         <button
           onClick={() => {
             setCurrentPage(previousPage);
+            updateStorageValues();
           }}
         >
           back
--- src/helpers/localStorage.tsx
+++ src/helpers/localStorage.tsx
@@ -1,8 +1,16 @@
-export const saveAsFavourites = (number: number) => {
+export const getLocalStorageValue = () => {
   let savedIdsArray: number[];
   const savedIds = localStorage.getItem("ids");
   if (savedIds != null) {
     savedIdsArray = JSON.parse(savedIds);
+    return savedIdsArray;
+  } else {
+    return null;
+  }
+};
+export const saveAsFavourites = (number: number) => {
+  const savedIdsArray = getLocalStorageValue();
+  if (savedIdsArray != null) {
     if (!savedIdsArray.includes(number)) {
       savedIdsArray.push(number);
       localStorage.setItem("ids", JSON.stringify(savedIdsArray));
@@ -12,10 +20,8 @@ export const saveAsFavourites = (number: number) => {
   }
 };
 export const removeFromFavourites = (number: number) => {
-  let savedIdsArray: number[];
-  const savedIds = localStorage.getItem("ids");
-  if (savedIds != null) {
-    savedIdsArray = JSON.parse(savedIds);
+  const savedIdsArray = getLocalStorageValue();
+  if (savedIdsArray != null) {
     const index = savedIdsArray.indexOf(number);
     if (index !== -1) {
       savedIdsArray.splice(index, 1);
